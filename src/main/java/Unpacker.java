67476665fa8b9bd38b8138667ce77e8cc5219e7c
fgavin <fgavin@student.21-school.ru>
2021-03-12 23:43:48 +0300

    Validate: checkNumbersBeforeBrackets(), checkBrackets(), checkValidChars()

--- src/main/java/Unpacker.java
+++ src/main/java/Unpacker.java
@@ -1,8 +1,80 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @author Andrey Fyodorov
  * Created on 12.03.2021.
  */
 
 public class Unpacker {
+
+  private String inputString;
+  private Validator validator;
+
+  public Unpacker() {
+    validator = new Validator();
+  }
+
+  public Unpacker(String inputString) {
+    this();
+    this.inputString = inputString;
+  }
+
+  public String getInputString() {
+    return inputString;
+  }
+
+  public void setInputString(String inputString) {
+    this.inputString = inputString;
+  }
+
+  public Validator getValidator() {
+    return validator;
+  }
+
+  class Validator {
+
+    public void validate() {
+      checkValidChars();
+      checkBrackets();
+      checkNumbersBeforeBrackets();
+    }
+
+    protected void checkValidChars() {
+        if (inputString.matches(".*[^\\w\\[\\]].*")) {
+          throw new IllegalArgumentException("Input string must contain only Latin letters, numbers and box brackets.");
+        }
+      }
+
+      protected void checkBrackets() {
+        int balance = 0;
+
+        for (int i = 0; i < inputString.length(); i++) {
+          if (inputString.charAt(i) == '[') {
+            ++balance;
+          } else if (inputString.charAt(i) == ']') {
+            --balance;
+            if (balance < 0 ) {
+              throw new IllegalArgumentException("Invalid bracket in input string at: " + (i + 1));
+            }
+          }
+        }
+
+        if (balance > 0) {
+          throw new IllegalArgumentException("Unbalanced brackets");
+        }
+      }
+
+      protected void checkNumbersBeforeBrackets() {
+        Pattern pattern = Pattern.compile("(\\d*)(\\[)");
+        Matcher matcher = pattern.matcher(inputString);
+
+        while (matcher.find()) {
+          if (matcher.group(1).length() == 0) {
+            throw new IllegalArgumentException("Number must precede square brackets");
+          }
+        }
+      }
+    }
 
 }
---
+++ src/test/java/UnpackerTest.java
@@ -0,0 +1,67 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+class UnpackerTest {
+
+  static Unpacker unpacker;
+
+  @BeforeAll
+  static void init() {
+	unpacker = new Unpacker();
+  }
+
+  @Test
+  public void validate_valid() {
+	unpacker.setInputString("3[xy3[ab]z]4[xy]z");
+	unpacker.getValidator().validate();
+  }
+
+  @Test
+  public void checkValidChars_containCyrillicX() {
+	unpacker.setInputString("3[хyz]4[xy]z");
+	Assertions.assertThrows(IllegalArgumentException.class, unpacker.getValidator()::checkValidChars);
+  }
+
+  @Test
+  public void checkValidChars_containPoint() {
+	unpacker.setInputString("3[xyz]4[x.y]z");
+	Assertions.assertThrows(IllegalArgumentException.class, unpacker.getValidator()::checkValidChars);
+  }
+
+  @Test
+  public void checkBrackets_startWithCloseBracket() {
+	unpacker.setInputString("3][хyz]4[xy]z");
+	Assertions.assertThrows(IllegalArgumentException.class, unpacker.getValidator()::checkBrackets);
+  }
+
+  @Test
+  public void checkBrackets_endWithOpenBracket() {
+	unpacker.setInputString("3[хyz]4[xy][z");
+	Assertions.assertThrows(IllegalArgumentException.class, unpacker.getValidator()::checkBrackets);
+  }
+
+  @Test
+  public void checkBrackets_unbalanced_1() {
+	unpacker.setInputString("3[хy[[z]4[xy]z");
+	Assertions.assertThrows(IllegalArgumentException.class, unpacker.getValidator()::checkBrackets);
+  }
+
+  @Test
+  public void checkBrackets_unbalanced_2() {
+	unpacker.setInputString("3[хyz]4[xy]zabs4[assd2[aas]");
+	Assertions.assertThrows(IllegalArgumentException.class, unpacker.getValidator()::checkBrackets);
+  }
+
+  @Test
+  public void checkNumbersBeforeBrackets_() {
+	unpacker.setInputString("3[хyz][xy]zabs4[assd2[aas]");
+	Assertions.assertThrows(IllegalArgumentException.class, unpacker.getValidator()::checkNumbersBeforeBrackets);
+  }
+
+  @Test
+  public void checkNumbersBeforeBrackets_nestedBracketsWithoutNumber() {
+	unpacker.setInputString("3[хyz]3[xy]zabs4[as[a]sd2[aas]");
+	Assertions.assertThrows(IllegalArgumentException.class, unpacker.getValidator()::checkNumbersBeforeBrackets);
+  }
+}
