1941a756ad60d0e03d6041ba96fdce799c258fb1
fgavin <fgavin@student.21-school.ru>
2021-03-14 19:58:44 +0300

    Change tests to testFactory

--- src/test/java/UnpackerTest.java
+++ src/test/java/UnpackerTest.java
@@ -1,50 +1,49 @@
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 class UnpackerTest {
 
-  static Unpacker unpacker;
+  private static final String[][] validCases;
 
-  static Map<String, String> validCases = new HashMap<>();
-
-  @BeforeAll
-  static void init() {
-	unpacker = new Unpacker();
-
-	validCases.put("3[xyz]4[xy]z", "xyzxyzxyzxyxyxyxyz");
-	validCases.put("2[3[x]y]", "xxxyxxxy");
-	validCases.put("", "");
-	validCases.put("x", "x");
-	validCases.put("xyz", "xyz");
-	validCases.put("1[x]", "x");
-	validCases.put("2[x]", "xx");
-	validCases.put("123[x]", "x".repeat(123));
-	validCases.put("40[x25[ab]y]", ("x" + "ab".repeat(25) + "y").repeat(40));
-	validCases.put("xyz12[ab]", "xyz" + "ab".repeat(12));
-	validCases.put("12[ab]xyz", "ab".repeat(12) + "xyz");
-	validCases.put("xyz12[ab]xyz", "xyz" + "ab".repeat(12) + "xyz");
-	//	validCases.put("", "");
+  static  {
+	validCases = new String[][]{
+		{"Test case 1 from task", "3[xyz]4[xy]z", "xyzxyzxyzxyxyxyxyz"},
+		{"Test case 2 from task", "2[3[x]y]", "xxxyxxxy"},
+		{"Empty string", "", ""},
+		{"One character string", "x", "x"},
+		{"Three characters string", "xyz", "xyz"},
+		{"Single repeat of one character", "1[x]", "x"},
+		{"Double repetition of one character", "2[x]", "xx"},
+		{"Three-digit number of repetition of one character", "123[x]", "x".repeat(123)},
+		{"Brackets at the end of the string", "xyz12[ab]", "xyz" + "ab".repeat(12)},
+		{"Brackets at the beginning of the string", "12[ab]xyz", "ab".repeat(12) + "xyz"},
+		{"Brackets in the middle of the string", "xyz12[ab]xyz", "xyz" + "ab".repeat(12) + "xyz"},
+		{"Double nested brackets", "40[x25[ab]y]", ("x" + "ab".repeat(25) + "y").repeat(40)},
+		{"Triple nested brackets", "xyz12[a124[cv]b]xyz", "xyz" + ("a" + "cv".repeat(124) + "b").repeat(12) + "xyz"}
+	};
   }
 
-  @Test
-  public void unpackRecursionRegEx_validCases() {
-	for (Map.Entry<String, String> stringStringEntry : validCases.entrySet()) {
-	  unpacker.setInputString(stringStringEntry.getKey());
-	  Assertions.assertEquals(stringStringEntry.getValue(), unpacker.unpackRecursion());
+  @TestFactory
+  Iterable<DynamicTest> dynamicTestIterable_unpackRecursion_validCases() {
+    List<DynamicTest> testList = new ArrayList<>();
+
+	for (String[] curCase : validCases) {
+	  testList.add(DynamicTest.dynamicTest(curCase[0],
+		  () -> Assertions.assertEquals(curCase[2], new Unpacker(curCase[1]).unpackRecursion())));
 	}
+	return testList;
   }
 
-  @Test
-  public void unpackIterative_validCases() {
-	for (Map.Entry<String, String> stringStringEntry : validCases.entrySet()) {
-	  unpacker.setInputString(stringStringEntry.getKey());
-	  unpacker.unpackIterative();
-	  Assertions.assertEquals(stringStringEntry.getValue(), unpacker.unpackIterative());
+  @TestFactory
+  Iterable<DynamicTest> dynamicTestIterable_unpackIterative_validCases() {
+	List<DynamicTest> testList = new ArrayList<>();
+
+	for (String[] curCase : validCases) {
+	  testList.add(DynamicTest.dynamicTest(curCase[0],
+		  () -> Assertions.assertEquals(curCase[2], new Unpacker(curCase[1]).unpackIterative())));
 	}
+	return testList;
   }
 
 }
--- src/test/java/ValidatorTest.java
+++ src/test/java/ValidatorTest.java
@@ -1,32 +1,41 @@
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
 
 import java.util.ArrayList;
 import java.util.List;
 
 class ValidatorTest {
 
-  @Test
-  public void unpack_invalidCases() {
-	List<String> invalidCases = new ArrayList<>();
-	invalidCases.add("2");
-	invalidCases.add("[xy]");
-	invalidCases.add("0[xy]");
-	invalidCases.add("3[хyz]4[xy]z");			//Cyrillic 'x'
-	invalidCases.add("3[xy3[ab]z]4[x_y]z");
-	invalidCases.add("3[xy3[ab]z]4[xüy]z");
-	invalidCases.add("3][хyz]4[xy]z");
-	invalidCases.add("3][хyz]4[xy]z");
-	invalidCases.add("3[хyz]4[xy][z");
-	invalidCases.add("3[хy[[z]4[xy]z");
-	invalidCases.add("3[хyz]4[xy]zabs4[assd2[aas]");
-	invalidCases.add("3[хyz][xy]zabs4[assd2[aas]");
-	invalidCases.add("3[хyz]3[xy]zabs4[as[a]sd2[aas]");
-	invalidCases.add("3[хyz]3[xy]zabs4[as0[a]sd2[aas]");
-//	invalidCases.add("");
+  private static final String[][] invalidCases;
 
-	for (String invalidCase : invalidCases) {
-	  Assertions.assertThrows(IllegalArgumentException.class, () -> Validator.validate(invalidCase));
-	}
+  static {
+	invalidCases = new String[][]{
+		{"Number without brackets", "2"},
+		{"Brackets without number", "[xy]"},
+		{"Repeat zero times", "0[xy]"},
+		{"Incorrect symbol: х (cyrillic)", "3[хyz]4[xy]z"},
+		{"Incorrect symbol: _", "3[xy3[ab]z]4[x_y]z"},
+		{"Incorrect symbol: ü", "3[xy3[ab]z]4[xüy]z"},
+		{"Closing bracket without leading opening bracket", "3][хyz]4[xy]z"},
+		{"Opening bracket without closing bracket", "3[хyz]4[xy][z"},
+		{"Unbalanced brackets (two extra opening brackets)", "3[хy[[z]4[xy]z"},
+		{"Unbalanced brackets (one less closing bracket)", "3[хyz]4[xy]zabs4[assd2[aas]"},
+		{"Brackets in a row without a number", "3[хyz][xy]zabs4[assd2[aas]"},
+		{"Nested brackets without a number", "3[хyz]3[xy]zabs4[as[a]sd2[aas]"},
+		{"Nested repeat zero times", "3[хyz]3[xy]zabs4[as0[a]sd2[aas]"},
+	};
   }
+
+  @TestFactory
+  Iterable<DynamicTest> dynamicTestIterable_unpackRecursion_validCases() {
+	List<DynamicTest> testList = new ArrayList<>();
+
+	for (String[] curCase : invalidCases) {
+	  testList.add(DynamicTest.dynamicTest(curCase[0],
+		  () -> Assertions.assertThrows(IllegalArgumentException.class, () -> Validator.validate(curCase[1]))));
+	}
+	return testList;
+  }
+
 }
